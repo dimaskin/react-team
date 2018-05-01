74d3eee0b8dd263273af555f57a62d78fab11f0d
dimaskin <dimaskin-nv@yandex.ru>
2018-05-01 21:10:36 +0300

    add state.json

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,13 @@ import React, { Component } from 'react';
 import Nav from './components/Nav'
 import ProjectList from './components/ProjectList/'
 import './App.css';
-const projects = [
-  {
-    id: 1, 
-    title: "website projects", 
-    description: "design and build a custom website",
-    status: 'active',
-    members: []
-  }
-]
+
 
 class App extends Component {
+  state = {
+    projects: this.props.state.projects
+  };
+
   render() {
     return (
       <div className="app container"> 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import state from './state.json';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+
+ReactDOM.render(<App state={state}/>, document.getElementById('root'));
